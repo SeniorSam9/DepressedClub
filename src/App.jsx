97795a2bc5baff570ebc56518323f0cbeb972343
seniorsam9 <rxrld050@hotmail.com>
2025-01-22 04:10:55 +0300

    r

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { Header, LetterForm, Letters } from "./components/";
 import LettersContextProvider from "./contexts/LettersConetxt";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    function fetchUser() {
+      // check if user got an account on app mount
+      const user = localStorage.getItem("username");
+      if (!user) {
+        fetc;
+      }
+    }
+    fetchUser();
+  }, []);
   return (
     <div className="h-screen overflow-auto bg-[#212631] text-white">
       <LettersContextProvider>
